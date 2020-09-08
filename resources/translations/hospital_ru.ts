2bbb447a346448c93639a3e43a0e3088a64dffb4
Anton Yarychkivsky <yarychkivsky@nixsolutions.com>
2020-09-08 16:26:38 +0300

    Add translation for Russian language

--- resources/translations/hospital_ru.ts
+++ resources/translations/hospital_ru.ts
@@ -4,32 +4,72 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <source>MainWindow</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Patient</source>
-        <translation>Пациент</translation>
-    </message>
-    <message>
-        <source>Remove</source>
-        <translation>Удалить</translation>
-    </message>
-    <message>
-        <source>toolBar</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Выход</translation>
     </message>
     <message>
         <source>Exit application</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть приложение</translation>
     </message>
     <message>
         <source>Add patient</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить пациента</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Редактировать</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <source>Remove patient</source>
+        <translation>Удалить пациента</translation>
+    </message>
+    <message>
+        <source>Remove patient from DataBase</source>
+        <translation>Удалить пациента из БД</translation>
+    </message>
+    <message>
+        <source>Add new patient to DataBase</source>
+        <translation>Добавить нового пациента в БД</translation>
+    </message>
+    <message>
+        <source>Open DataBase</source>
+        <translation>Открыть Базу данных</translation>
+    </message>
+    <message>
+        <source>Close DataBase</source>
+        <translation>Закрыть Базу данных</translation>
+    </message>
+    <message>
+        <source>Close connection to DataBase</source>
+        <translation>Закрыть соединение с БД</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>Английский</translation>
+    </message>
+    <message>
+        <source>Russian</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <source>Hospital DB Manager</source>
+        <translation>Менеджер БД пациентов</translation>
     </message>
 </context>
 </TS>
